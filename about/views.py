0abc4af03b0a469af6f2e2693d2221cd8a15a2e4
srimalisa563 <106435134+srimalisa563@users.noreply.github.com>
2022-09-28 20:46:07 -0800

    Add files via upload

---
+++ about/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {
+        'judul' : 'Universitas Sultan Ageng Tirtayasa',
+        'subjudul' : 'Welcome to Jawara University',
+    }
+    return render(request,'index.html',context)
+    
